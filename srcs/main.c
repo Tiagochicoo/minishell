0b68bcc151d7e9076a98cd01d0b674583027c1f6
Tiago Pereira <tpereira@c2r3s3.42lisboa.com>
2022-09-27 19:35:58 +0100

    save dell

--- srcs/main.c
+++ srcs/main.c
@@ -18,36 +18,33 @@ int	main(int argc, char **argv, char **envp)
 {
 	int	i;
 
-	i = 0;
-	if (argc == 1)
+
+	while (argc)
 	{
 		data.argv2 = argv;
 		data.env = *envp;
 		data.pwd = getcwd(NULL, 0);
 		data.rel_pwd = ft_strrchr(data.pwd, '/');
 		data.rel_pwd++;
-		data.buffer = readline(GREEN "minishell" RESET " " YELLOW "~" RESET " ");
-		data.cmds = ft_split(data.buffer, ' ');
-		data.cmd_count = ft_word_count(data.cmds[0], ' ');
-		printf("cmd_count: %d\n", data.cmd_count);
-		printf("pwd: %s\n", data.pwd);
-		printf("rel_pwd: %s\n", data.rel_pwd);
-		printf("buffer: %s\n", data.buffer);
-		while (i < data.cmd_count && data.cmds != NULL && data.cmds[i] != NULL)
-		{
-			printf("cmds[%d]: %s\n", i, data.cmds[i]);
-			i++;
-		}
-	}
-
-	while (1)
-	{
 		if (ft_strlen(data.buffer) > 0)
 		{
 			add_history(data.buffer);
 			free(data.buffer);
 		}
-		data.buffer = readline(GREEN "minishell" RESET " " YELLOW "~" RESET " ");
+		while (ft_strlen(data.buffer) == 0)
+			data.buffer = readline(GREEN "minishell" RESET " " YELLOW "~" RESET " ");
+		data.cmds = ft_split(data.buffer, ';');
+		data.cmd_count = ft_word_count(data.cmds[0], ' ');
+		printf("cmd_count: %d\n", data.cmd_count);
+		printf("pwd: %s\n", data.pwd);
+		printf("rel_pwd: %s\n", data.rel_pwd);
+		printf("buffer: %s\n\n", data.buffer);
+		i = 0;
+		while (i < data.cmd_count)
+		{
+			printf("cmds[%d]: %s\n", i, data.cmds[i]);
+			i++;
+		}
 		if (strcmp(data.buffer, "exit") == 0)
 			break ;
 		if (data.buffer[0] != '\0')
