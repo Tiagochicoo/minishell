1a7c26f5b86c27fd5ad999a376c1be290207c5e2
tpereira <tpereira@student.42lisboa.com>
2022-10-26 21:22:38 +0100

    fixed libft function

--- libft/srcs/ft_strncpy.c
+++ libft/srcs/ft_strncpy.c
@@ -6,23 +6,23 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 17:27:22 by tpereira          #+#    #+#             */
-/*   Updated: 2022/10/23 17:27:30 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/10/26 21:22:18 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, const char *src, unsigned int size)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < size)
 	{
 		dest[i] = src[i];
 		++i;
 	}
-	while (i < n)
+	while (i < size)
 	{
 		dest[i] = '\0';
 		i++;
