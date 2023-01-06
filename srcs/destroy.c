458eaafaf7495a786566b8f7e2f605e74a4b5952
Tiago Pereira <tpereira@c1r9s1.42lisboa.com>
2023-01-06 15:13:10 +0000

    save

---
+++ srcs/destroy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpereira <tpereira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 16:55:43 by tpereira          #+#    #+#             */
+/*   Updated: 2023/01/06 15:12:46 by tpereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void    ft_destroy_cmd(t_command *cmd)
+{
+	//ft_free_split(cmd->argv);
+	//ft_free_split(cmd->envp);
+	free(cmd->head);
+}
+
+void    ft_free_cmd(t_command *cmd)
+{
+	while (cmd->next)
+	{
+		ft_destroy_cmd(cmd);
+		cmd = cmd->next;
+	}
+	ft_destroy_cmd(cmd);
+}
